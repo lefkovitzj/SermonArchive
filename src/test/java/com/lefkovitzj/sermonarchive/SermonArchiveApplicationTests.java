52fd998bc09d7177fcd6634dc73c3ad4e5e001c8
Joseph Lefkovitz <85521235+lefkovitzj@users.noreply.github.com>
2026-04-17 12:23:31 -0400

    Initial project creation

---
+++ src/test/java/com/lefkovitzj/sermonarchive/SermonArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lefkovitzj.sermonarchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SermonArchiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
