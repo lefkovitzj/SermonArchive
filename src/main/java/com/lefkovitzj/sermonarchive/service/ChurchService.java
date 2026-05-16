c65a7388f3767e310b95d27b6c9a4bd2bda56e13
Joseph Lefkovitz <85521235+lefkovitzj@users.noreply.github.com>
2026-05-16 13:35:03 -0400

    Add Church object existence and ownership validation service methods

--- src/main/java/com/lefkovitzj/sermonarchive/service/ChurchService.java
+++ src/main/java/com/lefkovitzj/sermonarchive/service/ChurchService.java
@@ -6,6 +6,7 @@ import com.lefkovitzj.sermonarchive.entity.User;
 import com.lefkovitzj.sermonarchive.repository.ChurchRepository;
 import com.lefkovitzj.sermonarchive.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +15,12 @@ import java.util.List;
 @Service public class ChurchService {
     @Autowired
     private ChurchRepository  churchRepository;
+    private UserService userService;
+
+    public ChurchService(UserService userService) {
+        this.userService = userService;
+    }
+
 
     public List<Church> getChurches() {
         return churchRepository.findAll();
@@ -28,6 +35,22 @@ import java.util.List;
         newChurch.name = newName;
         newChurch.owner = currentUser;
         churchRepository.save(newChurch);
+    }
+
+    public boolean churchExists(String churchName) {
+        Church church = getChurchByName(churchName);
+        return church != null;
+    }
+
+    public boolean verifyOwnership(String churchName, UserDetails userDetails) {
+        User currentUser =  userService.getByName(userDetails.getUsername());
+        Church church = getChurchByName(churchName);
+        if (church == null || currentUser == null) {
+            return false;
+        }
+        System.out.println(church.owner);
+        System.out.println(currentUser);
+        return church.owner == currentUser;
     }
 
 }
