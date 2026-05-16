b4715885a93e6f42ce1cc55449e31139d81547ee
Joseph Lefkovitz <85521235+lefkovitzj@users.noreply.github.com>
2026-05-15 22:46:45 -0400

    Add speaker object

---
+++ src/main/java/com/lefkovitzj/sermonarchive/entity/Speaker.java
@@ -0,0 +1,37 @@
+package com.lefkovitzj.sermonarchive.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Entity
+public class Speaker {
+    @Id
+    @Column(unique = true)
+    private String name;
+
+    public Speaker() {}
+
+    public Speaker(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Speaker speaker = (Speaker) o;
+        return Objects.equals(name, speaker.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
