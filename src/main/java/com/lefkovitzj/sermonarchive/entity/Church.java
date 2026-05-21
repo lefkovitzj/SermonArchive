e0ce708b85f3a1c7ec2e48ba458075845b94ee70
Joseph Lefkovitz <85521235+lefkovitzj@users.noreply.github.com>
2026-05-21 11:40:28 -0400

    Clean up the Church entity with Lombok annotations

--- src/main/java/com/lefkovitzj/sermonarchive/entity/Church.java
+++ src/main/java/com/lefkovitzj/sermonarchive/entity/Church.java
@@ -1,15 +1,23 @@
 package com.lefkovitzj.sermonarchive.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
-import java.util.Objects;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 public class Church {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "Internal object ID representing the church.")
     public Integer id;
+    @Schema(description = "The legal or common name of the church.")
     public String name;
 
     @OneToMany(cascade = CascadeType.ALL)
@@ -17,25 +25,4 @@ public class Church {
 
     @ManyToOne(cascade = CascadeType.ALL)
     public User owner;
-
-    public Church() {}
-
-    public Church(int id, String name, List<Speaker> speakers, User owner) {
-        this.id = id;
-        this.name = name;
-        this.speakers = speakers;
-        this.owner = owner;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Church churches = (Church) o;
-        return id == churches.id && Objects.equals(name, churches.name) && Objects.equals(speakers, churches.speakers) && Objects.equals(owner, churches.owner);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, speakers, owner);
-    }
 }
