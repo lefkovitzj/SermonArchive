b6dfd3e5663d37f30db22d6e78b38a54a88ef4e5
Joseph Lefkovitz <85521235+lefkovitzj@users.noreply.github.com>
2026-05-21 11:43:11 -0400

    Clean up the SermonMedia entity with Lombok annotations

--- src/main/java/com/lefkovitzj/sermonarchive/entity/SermonMedia.java
+++ src/main/java/com/lefkovitzj/sermonarchive/entity/SermonMedia.java
@@ -1,7 +1,10 @@
 package com.lefkovitzj.sermonarchive.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -9,65 +12,76 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Entity
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 public class SermonMedia {
     @Id
+    @Getter
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "Internal object ID representing the sermon media.")
     private Integer id;
 
     @Setter
     @Getter
+    @Schema(description = "The title of the sermon.")
     private String title;
 
     @Setter
     @Getter
+    @Schema(description = "The full-length description of the sermon.")
     private String description;
 
     @Setter
     @Getter
+    @Schema(description = "The S3 object store public access URL for the sermon, if S3 is configured for public access.")
     private String resourceUrl;
 
     @Setter
     @Getter
+    @Schema(description = "The speaker who preached the sermon.")
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private Speaker speaker;
 
     @Setter
     @Getter
+    @Schema(description = "The church where the sermon was preached, as determined by the church it was uploaded by.")
     @ManyToOne(cascade = CascadeType.ALL)
     private Church church;
 
     @Setter
     @Getter
+    @Schema(description = "A list of tags associated with the sermon.")
     private List<String> tags =  new ArrayList<>();
 
     @Setter
     @Getter
+    @Schema(description = "Media is a video if true, audio otherwise.")
     private boolean isVideo;
 
     @Setter
     @Getter
+    @Schema(description = "Media is a public if true, private otherwise.")
     private boolean isPublished;
 
     @Setter
     @Getter
+    @Schema(description = "The time at which the sermon was preached.")
     private LocalDateTime sermonDatetime;
 
     @Setter
     @Getter
-    public String s3Key;
+    @Schema(description = "The media access key for the sermon in the S3 object store.")
+    private String s3Key;
 
     @CreationTimestamp
+    @Schema(description = "Internal system upload time.")
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
+    @Schema(description = "Internal system lastmod time.")
     private LocalDateTime updatedAt;
-
-
-    public SermonMedia() {
-    }
 
     public SermonMedia(Integer id, String title, String description, String resourceUrl, Speaker speaker, List<String> tags, boolean isVideo, boolean isPublished, String s3Key) {
         this.id = id;
@@ -79,18 +93,6 @@ public class SermonMedia {
         this.isVideo = isVideo;
         this.isPublished = isPublished;
         this.s3Key = s3Key;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        SermonMedia that = (SermonMedia) o;
-        return isVideo == that.isVideo && isPublished == that.isPublished && Objects.equals(id, that.id) && Objects.equals(title, that.title) && Objects.equals(description, that.description) && Objects.equals(resourceUrl, that.resourceUrl) && Objects.equals(speaker, that.speaker) && Objects.equals(church, that.church) && Objects.equals(tags, that.tags) && Objects.equals(sermonDatetime, that.sermonDatetime) && Objects.equals(s3Key, that.s3Key) && Objects.equals(createdAt, that.createdAt) && Objects.equals(updatedAt, that.updatedAt);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, title, description, resourceUrl, speaker, church, tags, isVideo, isPublished, sermonDatetime, s3Key, createdAt, updatedAt);
     }
 
     public void appendTag(String tag) {
