2950e7cc4c566e9b2f1e671a28918ef411894bb9
Joseph Lefkovitz <85521235+lefkovitzj@users.noreply.github.com>
2026-05-21 11:41:17 -0400

    Clean up the User entity with Lombok annotations

--- src/main/java/com/lefkovitzj/sermonarchive/entity/User.java
+++ src/main/java/com/lefkovitzj/sermonarchive/entity/User.java
@@ -1,76 +1,36 @@
 package com.lefkovitzj.sermonarchive.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
-import java.util.Objects;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 @Table(name="app_users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter
     private Integer id;
+
     @Column(unique = true)
+    @Setter
+    @Getter
     private String username;
+
+    @Setter
+    @Getter
     private String password;
+
+    @Setter
+    @Getter
     private List<String> roles;
+
+    @Setter
+    @Getter
     @Column(unique = true)
     private String email;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public List<String> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<String> roles) {
-        this.roles = roles;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(password, user.password) && Objects.equals(roles, user.roles) && Objects.equals(email, user.email);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, username, password, roles, email);
-    }
 }
